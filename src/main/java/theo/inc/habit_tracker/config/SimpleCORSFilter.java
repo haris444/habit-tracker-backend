5187ce50fa9db77502d1733ddb255113e9564565
theo <haris444.hv@gmail.com>
2025-03-13 18:05:04 +0200

    SimpleCorsFilter-Updated

--- src/main/java/theo/inc/habit_tracker/config/SimpleCORSFilter.java
+++ src/main/java/theo/inc/habit_tracker/config/SimpleCORSFilter.java
@@ -3,6 +3,7 @@ package theo.inc.habit_tracker.config;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -17,11 +18,24 @@ public class SimpleCORSFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
-        httpResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:3000, https://theo-habit-tracker.netlify.app");
+
+        String origin = httpRequest.getHeader("Origin");
+        if (origin != null && (origin.equals("http://localhost:3000") || origin.equals("https://theo-habit-tracker.netlify.app"))) {
+            httpResponse.setHeader("Access-Control-Allow-Origin", origin);
+        }
+
         httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         httpResponse.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
         httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
+
+        // Handle preflight OPTIONS requests
+        if ("OPTIONS".equalsIgnoreCase(httpRequest.getMethod())) {
+            httpResponse.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+
         chain.doFilter(request, response);
     }
 
