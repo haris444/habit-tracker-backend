79e51ddaa615aad125af033ab2e8a70213bb62f5
theo <haris444.hv@gmail.com>
2025-03-16 16:11:18 +0200

    Fix default package ComponentScan and exclude R2DBC autoconfiguration

--- src/main/java/theo/inc/habit_tracker/HabitTrackerApplication.java
+++ src/main/java/theo/inc/habit_tracker/HabitTrackerApplication.java
@@ -1,3 +1,5 @@
+package theo.inc.habit_tracker;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
